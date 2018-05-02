6e3b6138d8855d44c8d4c5e78e04293b6d8458e2
danip293 <danip293@gmail.com>
2018-05-02 13:51:24 -0500

    delete folder api

--- galery/src/api/index.js
+++
@@ -1,7 +0,0 @@
-const url = "http://api.tvmaze.com/schedule?country=US&date=2018-03-13";
-export default () =>{
-	return fetch(url)
-		.then(response => Promise.all([response, response.json()]))	
-	
-
-} 
